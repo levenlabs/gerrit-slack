bed99c83e2ad0ccc12bffc9464f47336ae294787
James Hartig <james@getadmiral.com>
2019-03-07 22:42:38 -0500

    Buffer by 1 second in case rounding

--- events/reviewerAdded.go
+++ events/reviewerAdded.go
@@ -27,9 +27,9 @@ func (ReviewerAdded) Ignore(e gerritssh.Event, pcfg project.Config) (bool, error
 		return true, nil
 	}
 	if !pcfg.PublishPatchSetReviewersAdded {
-		// if the event and the patchset were created at the same time, the reviewers
+		// if the event and the patchset were created within a second, the reviewers
 		// were added with the patchset
-		if e.PatchSet.TSCreated == e.TSCreated {
+		if e.TSCreated-e.PatchSet.TSCreated <= 1 {
 			return true, nil
 		}
 	}
